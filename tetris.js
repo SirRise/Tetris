7f2dd2e0825e673404c70036a5e2c533596b4a79
Gordon Fassbender <gordon.fassbender@gmx.de>
2018-01-04 22:06:44 +0100

    #

---
+++ tetris.js
@@ -0,0 +1,60 @@
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+context.scale(20, 20);
+
+class Player {
+    constructor(x, y, matrix) {
+        this.pos = {x: x, y: y}
+        this.matrix = matrix;
+    }
+}
+
+const pieces = ['I', 'L', 'J', 'O', 'Z', 'S', 'T'];
+
+function createPiece(type)
+{
+    if (type === 'I') {
+        return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ];
+    } else if (type === 'L') {
+        return [
+            [0, 2, 0],
+            [0, 2, 0],
+            [0, 2, 2],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [3, 3, 0],
+        ];
+    } else if (type === 'O') {
+        return [
+            [4, 4],
+            [4, 4],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [5, 5, 0],
+            [0, 5, 5],
+            [0, 0, 0],
+        ];
+    } else if (type === 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === 'T') {
+        return [
+            [0, 7, 0],
+            [7, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
